b195b51496a22a5fd861228bc4bef67e4d048c89
gl-coding <1451607278@qq.com>
2020-01-06 19:14:29 +0800

    update

--- server_for_amis.py
+++ server_for_amis.py
@@ -53,6 +53,32 @@ class FileUploadProcesser(tornado.web.RequestHandler):
     def get(self):
         self.render("templete_action_form.html")
 
+class TableProcesser(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templete_action_table.html")
+
+class TableApiProcesser(tornado.web.RequestHandler):
+    def get(self):
+        res = self.get_argument("args")
+        print res
+        res_json = { "status": 0, "msg": "ok", "data": { "rows": [{"engine":"1", "version":"2"}] } }
+        res_json = json.dumps(res_json)
+        print res_json
+        self.write(res_json)
+
+class AudioProcesser(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templete_action_audio.html")
+
+class AudioApiProcesser(tornado.web.RequestHandler):
+    def get(self):
+        res = self.get_argument("args")
+        print res
+        res_json = { "status": 0, "msg": "ok", "data": [{"engine":"1", "audio":"https://amis.bj.bcebos.com/amis/2019-7/1562137295708/chicane-poppiholla-original-radio-edit%20(1).mp3"}] }
+        res_json = json.dumps(res_json)
+        print res_json
+        self.write(res_json)
+
 def make_app():
     return tornado.web.Application([
         (r"/get",      GetHandler),
@@ -63,6 +89,10 @@ def make_app():
         (r"/form_pro", FormProcesser),
         (r"/file",     FileUploadHandler),
         (r"/file_pro", FileUploadProcesser),
+        (r"/table",    TableProcesser),
+        (r"/table_api",TableApiProcesser),
+        (r"/audio",    AudioProcesser),
+        (r"/audio_api",AudioApiProcesser),
         ],
         template_path = os.path.join(
             os.path.dirname(__file__), "templates" # 索引到templates文件夹中的html
